295698fa0a6e38116d30f698beed82be868ec3d6
Alexander Zhelyazkov <alexander.zhelyazkov@icloud.com>
2026-08-07 19:36:43 +0300

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,39 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # construct the graph
+        # for each node, store the in-degree
+        # if a node has an in-degree of zero, then it is reachable
+        
+        # process all the starting nodes (in-degree = zero)
+        # for all of their neighbors, reduce their in-degree
+
+        indegree = [0] * numCourses
+        graph = [[] for _ in range(numCourses)]
+
+        for src, dest in prerequisites:
+            indegree[dest] += 1
+            graph[src].append(dest)
+
+        q = deque()
+
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        seen = 0
+
+        while q:
+            vert = q.popleft()
+            seen += 1
+
+            for neigh in graph[vert]:
+                indegree[neigh] -= 1
+
+                if indegree[neigh] == 0:
+                    q.append(neigh)
+        
+        return seen == numCourses
+
+
+
+        
